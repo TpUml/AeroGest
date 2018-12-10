e756f9ab1e51e945ef4e78882979f3f93d0b28b4
tpUml <tpUml303@gmail.com>
2018-12-10 05:44:57 +0100

    Premier Commit

---
+++ src/main/java/org/aeroGest/AeroGestApplication.java
@@ -0,0 +1,12 @@
+package org.aeroGest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroGestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroGestApplication.class, args);
+	}
+}
